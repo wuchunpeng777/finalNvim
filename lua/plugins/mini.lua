e025f78412c6794ddfa87102c35655867d88244e
wuchunpeng777 <wcpwcp9@gmail.com>
2024-10-04 22:29:58 +0800

    mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -43,5 +43,27 @@ return {
         })
 
         require('mini.bufremove').setup()
+
+        -- Better Around/Inside textobjects
+        --
+        -- Examples:
+        --  - va)  - [V]isually select [A]round [)]paren
+        --  - yinq - [Y]ank [I]nside [N]ext [']quote
+        --  - ci'  - [C]hange [I]nside [']quote
+        require('mini.ai').setup {
+            n_lines = 500
+        }
+
+        -- Add/delete/replace surroundings (brackets, quotes, etc.)
+        --
+        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+        -- - sd'   - [S]urround [D]elete [']quotes
+        -- - sr)'  - [S]urround [R]eplace [)] [']
+        require('mini.surround').setup()
+
+        require('mini.files').setup({})
+        vim.keymap.set('n', '<leader>fi', '<cmd>lua MiniFiles.open()<cr>')
+
+        require('mini.cursorword').setup()
     end
 }
