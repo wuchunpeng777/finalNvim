03908cfbad749c9f9a45a69e272cad0d86ade34e
wuchunpeng <wcpwcp9@gmail.com>
2024-10-05 22:59:16 +0800

    bookmark

--- lua/plugins/bookmark.lua
+++ lua/plugins/bookmark.lua
@@ -4,14 +4,11 @@ return {
         vim.keymap.set({"n", "v"}, "mm", "<cmd>BookmarksMark<cr>", {
             desc = "Mark current line into active BookmarkList."
         })
-        vim.keymap.set({"n", "v"}, "mo", "<cmd>BookmarksGoto<cr>", {
+        vim.keymap.set({"n", "v"}, "mf", "<cmd>BookmarksGoto<cr>", {
             desc = "Go to bookmark at current active BookmarkList"
         })
         vim.keymap.set({"n", "v"}, "ma", "<cmd>BookmarksCommands<cr>", {
             desc = "Find and trigger a bookmark command."
-        })
-        vim.keymap.set({"n", "v"}, "mg", "<cmd>BookmarksGotoRecent<cr>", {
-            desc = "Go to latest visited/created Bookmark"
         })
 
         require("bookmarks").setup({
