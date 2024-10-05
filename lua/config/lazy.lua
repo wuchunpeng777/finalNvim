49f6668481792dc1d2495c5896ce9bbe2062a13c
wuchunpeng <wcpwcp9@gmail.com>
2024-10-05 23:09:35 +0800

    文件夹整理

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,11 @@ require("lazy").setup({
     spec = { -- import your plugins
     {
         import = "plugins"
-    }},
+    },
+    {
+        import = "plugins.theme"
+    }
+  },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     -- install = { colorscheme = { "habamax" } },
