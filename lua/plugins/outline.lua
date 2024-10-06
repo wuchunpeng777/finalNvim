b2f8b816b11f2fefe962d720cfaa301b94e92f0e
wuchunpeng <wcpwcp9@gmail.com>
2024-10-06 21:55:56 +0800

    outline keymap

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -2,7 +2,7 @@ return{
     "hedyhli/outline.nvim",
     config = function()
       -- Example mapping to toggle outline
-      vim.keymap.set("n", "<leader>ot", "<cmd>Outline<CR>",
+      vim.keymap.set("n", "<leader>lS", "<cmd>Outline<CR>",
         { desc = "Toggle Outline" })
   
       require("outline").setup {
