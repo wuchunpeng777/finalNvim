d2594f27890602e14633fa0a252f07e7bd30f54a
wuchunpeng <wcpwcp9@gmail.com>
2024-10-06 22:20:38 +0800

    autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,16 +1,18 @@
 return {
-    "Pocco81/auto-save.nvim",
+    "okuuva/auto-save.nvim",
     config = function()
         require("auto-save").setup {
             -- your config goes here
             -- or just leave it empty :)
             execution_message = {
+                enabled = false,
                 message = function() -- message to print on save
                     return ''
                 end,
-                dim = 0.18, -- dim the color of `message`
-                cleaning_interval = 0 -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-            }
+                dim = 0,   -- dim the color of `message`
+                cleaning_interval = 0, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+            },
+            debounce_delay = 50
         }
     end
 }
