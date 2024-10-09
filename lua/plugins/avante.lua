e7c35ab8ab515375482b630871f812d45c20035d
wuchunpeng777 <wcpwcp9@gmail.com>
2024-10-09 15:57:23 +0800

    区分平台

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,43 +1,87 @@
-return {{
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    build = "make",
-    opts = {
-        -- add any opts hereby
-        provider = "openai",
-        openai = {
-            endpoint = "https://openrouter.ai/api/v1",
-            model = "anthropic/claude-3.5-sonnet",
-            temperature = 0
-        }
-    },
-    dependencies = {"nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim", "nvim-lua/plenary.nvim",
-                    "MunifTanjim/nui.nvim", --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
+if vim.fn.has "win32" == 1 then
+    return {{
+        "yetone/avante.nvim",
         event = "VeryLazy",
+        build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for window
         opts = {
-            -- recommended settings
-            default = {
-                embed_image_as_base64 = false,
-                prompt_for_file_name = false,
-                drag_and_drop = {
-                    insert_mode = true
-                },
-                -- required for Windows users
-                use_absolute_path = true
+            -- add any opts hereby
+            provider = "openai",
+            openai = {
+                endpoint = "https://openrouter.ai/api/v1",
+                model = "anthropic/claude-3.5-sonnet",
+                temperature = 0
             }
-        }
-    }, {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-            file_types = {"markdown", "Avante"}
         },
-        ft = {"markdown", "Avante"}
+        dependencies = {"nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim", "nvim-lua/plenary.nvim",
+                        "MunifTanjim/nui.nvim", --- The below dependencies are optional,
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true
+                }
+            }
+        }, {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = {"markdown", "Avante"}
+            },
+            ft = {"markdown", "Avante"}
+        }}
     }}
-}}
-
+else
+    return {{
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        build = "make",
+        opts = {
+            -- add any opts hereby
+            provider = "openai",
+            openai = {
+                endpoint = "https://openrouter.ai/api/v1",
+                model = "anthropic/claude-3.5-sonnet",
+                temperature = 0
+            }
+        },
+        dependencies = {"nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim", "nvim-lua/plenary.nvim",
+                        "MunifTanjim/nui.nvim", --- The below dependencies are optional,
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true
+                }
+            }
+        }, {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = {"markdown", "Avante"}
+            },
+            ft = {"markdown", "Avante"}
+        }}
+    }}
+end
