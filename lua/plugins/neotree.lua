40709ed179cc59aca63e735f200762792ccf2ce5
wuchunpeng777 <wcpwcp9@gmail.com>
2024-10-09 16:01:55 +0800

    neotree auto close

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -26,6 +26,7 @@ return {
         })
 
         require("neo-tree").setup({
+            auto_clean_after_session_restore = true,
             close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
             popup_border_style = "rounded",
             enable_git_status = true,
