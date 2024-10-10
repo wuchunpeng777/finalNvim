85bde326e5b2cd850d7519cc6a82489a76dae5c5
wuchunpeng777 <wcpwcp9@gmail.com>
2024-10-10 17:44:56 +0800

    hardtime

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim"},
+    opts = {},
+    config = function()
+        require("hardtime").setup({
+            -- Add "oil" to the disabled_filetypes
+            disabled_filetypes = {"qf", "netrw", "NvimTree", "lazy", "mason", "oil", "neo-tree"},
+        })
+    end
+}
