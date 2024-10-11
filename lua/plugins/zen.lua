d6b35b5d34840b1b6991080ecb10f5bb48b6feea
wuchunpeng777 <wcpwcp9@gmail.com>
2024-10-11 14:06:34 +0800

    hide statusline in zen mode

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -30,7 +30,7 @@ return {
                 showcmd = false, -- disables the command in the last line of the screen
                 -- you may turn on/off statusline in zen mode by setting 'laststatus' 
                 -- statusline will be shown only if 'laststatus' == 3
-                laststatus = 3 -- turn off the statusline in zen mode
+                laststatus = 0 -- turn off the statusline in zen mode
             },
             twilight = {
                 enabled = true
